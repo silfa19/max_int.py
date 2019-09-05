dedb0e528ebcb2413a1da49ef12e23cb49c8725f
Silfá Björk <silfa19@ru.is>
2019-09-05 15:04:02 +0000

    Algorithm written for sequence

---
+++ sequence.py
@@ -0,0 +1,16 @@
+fyrst að fatt hvað sequencein er
+
+#1+2+3 = 6 
+#2+3+6 = 11
+#3+6+11 = 20
+#6+11+20 = 37
+
+o.s.frv 
+Byrja á því að biðja um integer, integerið er hversu oft sequenceið á að vera skrifað. 
+Ég held að það eigi að nota forloop.
+af því við erum að plúsa saman þrjár tölur í sequenceinu gefum við þremur breytum gildin 1,2 og 3 
+Finna í hvaða rangei það ætti að vera í til að ganga upp 
+fá summu fyrir 1,2,3 og halda síðan áfram með rununa þar til það er komið upp í hversu oft við viljum að það keyri
+
+
+
