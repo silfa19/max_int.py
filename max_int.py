62fbea6707da65934b2a504bcebc86560cf3b289
Silfá Björk <silfa19@ru.is>
2019-09-05 14:05:35 +0000

    first commit

--- max_int.py
+++ max_int.py
@@ -1,3 +1,11 @@
+Skrifa kóða sem biður um integer þar til að neikvæð tala er slegin inn.
+Biðja fyrist um tölu frá notanda 
+Gera grein fyrir hvort talan sé jákvæð eða neikvæð
+Ef talan er jákvæð, halda áfram að biðja um tölu 
+Þegar talan verður neikvæð, gefa upp hvað hæsta integerið var. 
+
+
+
 
 num_int = int(input("Input a number: "))    # Do not change this line
 
