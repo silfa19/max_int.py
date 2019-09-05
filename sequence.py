24b2bc1af97c36cd6e93600b56750558ce01d221
Silfá Björk <silfa19@ru.is>
2019-09-05 15:20:13 +0000

    Algorithm written for sequence

--- sequence.py
+++ sequence.py
@@ -1,16 +1,9 @@
+1. Hvað er verkefnið 
+2. Búa til breytur með fyrstu 3 tölurnar
+3. búa til forloop
+4. prenta fyrstu þrjár tölurnar
+5. finna til leið til að prenta næstu þrjár tölur
+
+
 fyrst að fatt hvað sequencein er
-
-#1+2+3 = 6 
-#2+3+6 = 11
-#3+6+11 = 20
-#6+11+20 = 37
-
-o.s.frv 
-Byrja á því að biðja um integer, integerið er hversu oft sequenceið á að vera skrifað. 
-Ég held að það eigi að nota forloop.
-af því við erum að plúsa saman þrjár tölur í sequenceinu gefum við þremur breytum gildin 1,2 og 3 
-Finna í hvaða rangei það ætti að vera í til að ganga upp 
-fá summu fyrir 1,2,3 og halda síðan áfram með rununa þar til það er komið upp í hversu oft við viljum að það keyri
-
-
 
